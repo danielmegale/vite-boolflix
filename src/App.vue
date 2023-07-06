2aea28d866e9b88cfd6ec7a7166702c876ed83e3
danielmegale <danim1@live.it>
2023-07-06 12:47:18 +0200

    added poster and flags

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,52 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
-import {store} from './assets/data/store.js'
-import {movieEndpoint, tvEndpoint} from './assets/data';
+import { store } from './assets/data/store.js'
+import { movieEndpoint, tvEndpoint } from './assets/data';
 import axios from 'axios';
-export default{
-    components:{AppHeader,AppMain},
-    data(){
-      return{
-        store,
-        nameFilter:'',
-      }
+export default {
+    components: { AppHeader, AppMain },
+    data() {
+        return {
+            nameFilter: '',
+        }
     },
-    methods:{
-        getMovies(url){
-            axios.get(url).then((res)=>{
-                const apiMovies=res.data.results;
-                store.movies=apiMovies.map(movie=>{
-                     const {title,original_title,original_language,vote_average}=movie;
-                     return{title, original_title, original_language, vote_average};
+    methods: {
+        getMovies(url) {
+            axios.get(url).then((res) => {
+                const apiMovies = res.data.results;
+                store.movies = apiMovies.map(movie => {
+                    const { title, original_title, original_language, vote_average, poster_path } = movie;
+                    return { title, original_title, original_language, vote_average, poster_path };
                 });
             });
         },
-        getTvSeries(url){
-            axios.get(url).then((res)=>{
-                const apiTvSeries=res.data.results;
-                store.tvSeries=apiTvSeries.map(tv=>{
-                    const {original_language,original_name,name,vote_average}=tv;
-                    return{original_language,original_name,name,vote_average}
-                })                
+        getTvSeries(url) {
+            axios.get(url).then((res) => {
+                const apiTvSeries = res.data.results;
+                store.tvSeries = apiTvSeries.map(tv => {
+                    const { original_language, original_name, name, vote_average, poster_path } = tv;
+                    return { original_language, original_name, name, vote_average, poster_path }
+                })
             })
         },
-        onChangeTerm(term){
-            this.nameFilter=term;
+        onChangeTerm(term) {
+            this.nameFilter = term;
         },
-        searchMovies(){
-            const filterEndpoint=`${movieEndpoint}&query=${this.nameFilter}`
-            const tvSeriesEndpoint=`${tvEndpoint}&query=${this.nameFilter}`
+        searchMovies() {
+            const filterEndpoint = `${movieEndpoint}&query=${this.nameFilter}`
+            const tvSeriesEndpoint = `${tvEndpoint}&query=${this.nameFilter}`
             this.getMovies(filterEndpoint);
             this.getTvSeries(tvSeriesEndpoint)
         }
     }
-  
+
 }
 </script>
 
 <template>
-    <AppHeader @change-term="onChangeTerm" @form-submit="searchMovies"/>
-    <AppMain :store="store"/>
+    <AppHeader @change-term="onChangeTerm" @form-submit="searchMovies" />
+    <AppMain />
 </template>
 
-<style>
-
-</style>
+<style></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,39 @@
 <script>
-export default{
-    props:{
-        store:Object
+import { store } from '../assets/data/store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        getImgLanguage(target) {
+            const imgLanguage = new URL(`../assets/img/${target}.png`, import.meta.url);
+            return imgLanguage.href
+        },
     }
+
 }
 </script>
 
 <template>
-    <div v-show="store.movies.length > 0 ">
-        <div v-for="movie in store.movies">{{ movie.original_title }}</div>
+    <div v-show="store.movies.length > 0">
+        <ul v-for="movie in store.movies">
+            <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" alt="">
+            <li>{{ movie.original_title }}</li>
+            <li>
+                <img :src="getImgLanguage(movie.original_language)" alt="">
+            </li>
+        </ul>
         <h1>Tv Series</h1>
-        <div v-for="tv in store.tvSeries">{{ tv.original_name }}</div>
+        <ul v-for="tv in store.tvSeries">
+            <img :src="`https://image.tmdb.org/t/p/w342${tv.poster_path}`" alt="">
+            <li>{{ tv.original_name }}</li>
+            <li>
+                <img :src="getImgLanguage(tv.original_language)" alt="">
+            </li>
+        </ul>
     </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
