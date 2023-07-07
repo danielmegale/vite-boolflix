c2845989be698e9b6f18d11389ffbd293df10383
danielmegale <danim1@live.it>
2023-07-07 12:50:42 +0200

    added layout

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,47 +26,86 @@ export default {
 
 <template>
     <div class="container">
-        <div v-show="store.movies.length > 0" class="row flex-wrap">
-            <ul v-for="movie in store.movies" class="list-unstyled col-3 p-1 text-white">
-                <li>
+        <div v-show="store.movies.length > 0" class="row flex-wrap ">
+            <h1>MOVIES</h1>
+            <ul v-for="movie in store.movies" class="list-unstyled col-3 p-1 text-white position-relative">
+                <li class="poster">
                     <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" alt="">
                     <img v-else
                         src="https://w7.pngwing.com/pngs/280/326/png-transparent-logo-netflix-logos-and-brands-icon-thumbnail.png"
                         alt="">
                 </li>
-                <div>
-                    <li>{{ movie.title }}</li>
-                    <li>{{ movie.original_title }}</li>
+                <div class="position-absolute top-50 start-0 end-0 info-card text-center">
+                    <h2>{{ movie.title }}</h2>
+                    <h3>{{ movie.original_title }}</h3>
+                    <div class="flag-container">
+                        <img v-if="getImgLanguage(movie.original_language)" :src="getImgLanguage(movie.original_language)"
+                            :alt="movie.original_language">
+                        <span v-else>{{ (movie.original_language) }}</span>
+                    </div>
+                    <li>
+                        <i v-for="star of getRating(movie.vote_average)" class="fa-solid fa-star"></i>
+                        <i v-for="stars of (5 - getRating(movie.vote_average))" class="fa-regular fa-star"></i>
+                    </li>
                 </div>
-                <li>
-                    <img v-if="getImgLanguage(movie.original_language)" :src="getImgLanguage(movie.original_language)"
-                        :alt="movie.original_language">
-                    <span v-else>{{ (movie.original_language) }}</span>
-                </li>
-                <li>
-                    <i v-for="star of getRating(movie.vote_average)" class="fa-solid fa-star"></i>
-                    <i v-for="stars of (5 - getRating(movie.vote_average))" class="fa-regular fa-star"></i>
-                </li>
             </ul>
-            <h1>Tv Series</h1>
-            <ul v-for="tv in store.tvSeries" class="list-unstyled col-3">
-                <img v-if="tv.poster_path" :src="`https://image.tmdb.org/t/p/w342${tv.poster_path}`" alt="">
-                <img v-else
-                    src="https://w7.pngwing.com/pngs/280/326/png-transparent-logo-netflix-logos-and-brands-icon-thumbnail.png"
-                    alt="">
-                <li>{{ tv.original_name }}</li>
-                <li>
-                    <img v-if="getImgLanguage(tv.original_language)" :src="getImgLanguage(tv.original_language)"
-                        :alt="tv.original_language">
-                    <span v-else>{{ tv.original_language }}</span>
+            <h1>TV SERIES</h1>
+            <ul v-for="tv in store.tvSeries" class="list-unstyled col-3 p-1 text-white position-relative">
+                <li class="poster">
+                    <img v-if="tv.poster_path" :src="`https://image.tmdb.org/t/p/w342${tv.poster_path}`" alt="">
+                    <img v-else
+                        src="https://w7.pngwing.com/pngs/280/326/png-transparent-logo-netflix-logos-and-brands-icon-thumbnail.png"
+                        alt="">
                 </li>
-                <li>
-                    <i v-for="star of getRating(tv.vote_average)" class="fa-solid fa-star"></i>
-                    <i v-for="stars of (5 - getRating(tv.vote_average))" class="fa-regular fa-star"></i>
-                </li>
+                <div class="position-absolute top-50 start-0 end-0 info-card text-center">
+                    <h2>{{ tv.name }}</h2>
+                    <h3>{{ tv.original_name }}</h3>
+                    <div class="flag-container">
+                        <img v-if="getImgLanguage(tv.original_language)" :src="getImgLanguage(tv.original_language)"
+                            :alt="tv.original_language">
+                        <span v-else>{{ tv.original_language }}</span>
+                    </div>
+                    <li>
+                        <i v-for="star of getRating(tv.vote_average)" class="fa-solid fa-star"></i>
+                        <i v-for="stars of (5 - getRating(tv.vote_average))" class="fa-regular fa-star"></i>
+                    </li>
+                </div>
             </ul>
         </div>
     </div>
 </template>
 
-<style></style>
+<style lang=scss scoped>
+.flag-container {
+    width: 50px;
+    margin: 0 auto;
+
+    // img {
+    //     width: 50px;
+    // }
+}
+
+.info-card {
+    transform: translateY(-50%);
+    display: none;
+}
+
+.poster {
+    height: 100%;
+
+    img {
+        height: 100%;
+    }
+}
+
+
+
+ul.list-unstyled:hover .poster {
+    opacity: 0.5;
+}
+
+ul.list-unstyled:hover .info-card {
+    display: block;
+    opacity: 1;
+}
+</style>
